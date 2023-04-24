2fa1f00005ad647d1f04d7cbd59ed60a339b113d
erin <sht2316@autuni.ac.nz>
2023-04-24 17:09:33 +1200

    student

--- PDC-SC-COMP603/src/pdc/sc/comp603/Student.java
+++ PDC-SC-COMP603/src/pdc/sc/comp603/Student.java
@@ -12,6 +12,27 @@ public class Student {
     
     private String studentName;
     private String classCode;
+
+    public Student(String studentName, String classCode) {
+        this.studentName = studentName;
+        this.classCode = classCode;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getClassCode() {
+        return classCode;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setClassCode(String classCode) {
+        this.classCode = classCode;
+    }
     
     
     
